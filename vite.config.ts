445ed6b1321ce24507dacf0efdcf1c38c49d3afd
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-02-04 18:44:59 +0200

    Deployment commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/demetra-iteam/",
   plugins: [react(), macrosPlugin()],
 })
